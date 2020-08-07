877a4560a2d346294d90062046fd312e52b6209a
Petr Gargulak <petr.gargulak@nxp.com>
2020-08-07 09:28:59 +0200

    Predelane  (opet) linky na dabovane veci.

--- xml/get_lang.py
+++ xml/get_lang.py
@@ -1,26 +1,29 @@
 import xbmcgui, xbmcaddon
 
 languages = ("CS", "SK", "EN")
-new_dub_url = {"CS":
-                    {
-                        "CS":"DYQwdg5gvAxgzgMlJWi4HsBOAXKATEbAUwEE88i8FsBPAByKgFt0A3ASyISws3yLgwEBGnCgBmAGwBWIA",
-                        "EN":"DYQwdg5gvAxgzgMlJKBTMC4HsBOAXKAExD1QEFDDVCE8BPAB1SgFssA3AS1QVypyKo4MBMTpwoAZgBsAViA",
-                        "SK":"DYQwdg5gvAxgzgMlJKcDWC4HsBOAXKAExDwFMBBQw0whPATwAdSoBbLANwEtSFdqcRUnBgJi9OFADMANgCsQA"
-                    },
-               "EN":
-                    {
-                        "CS":"DYQwdg5gvApmBkpJQMYGd5oPYCcAuUAJiHjAIKGEyHx4CeADjFALZYBuAljPLlTkRhoU8YnTRQAzADYArEA",
-                        "EN":"DYQwdg5gvApmBkpKwQZwPYCcAuUAmI2MAgnnjHvNgJ4AOMUAtugG4CWM8W5m%2bMqAY3gFqqKAGYAbAFYgA",
-                        "SK":"DYQwdg5gvApmBkpJQM4Gt4oPYCcAuUAJiHjAIKGEyHx4CeADjFALZYBuAljPLlTkRgoAxvGJ0UUAMwA2AKxA"
-                    },
-               "SK":
-                    {
-                        "CS":"DYQwdg5gvAzg1gMlJKBjGCYHsBOAXKAExDwFMBBQw0whPATwAdSoBbLANwEtSFdqcRUjFQJi9GFADMANgCsQA",
-                        "EN":"DYQwdg5gvAzg1gMlJKBTMCYHsBOAXKAExD1QEFDDVCE8BPAB1SgFssA3AS1QVypyKoYAYwTE6MKAGYAbAFYgA",
-                        "SK":"DYQwdg5gvAzg1gMlJWiYHsBOAXKATEbAUwEE88i8FsBPAByKgFt0A3ASyISws3yJgBjBARowoAZgBsAViA"
-                    }
-              }
-        
+
+# new_dub_url = {"CS":
+#                     {
+#                         "CS":"DYQwdg5gvAxgzgMlJWi4HsBOAXKyIAiI2ApgIIAmFJFC2AngA4lQC26AbgJYkJbWYo1ODAQUQ9OFADMANgCsQA",
+#                         "EN":"DYQwdg5gvAxgzgMlJKBTMC4HsBOAXKZCAERD1QEEATK1KhPATwAdUoBbLANwEtUFctHFFpwYCKiEZwoAZgBsAViA",
+#                         "SK":"DYQwdg5gvAxgzgMlJKcDWC4HsBOAXKZCAERDwFMBBAE2vOoTwE8AHcqAWywDcBLchLjo4odODATUQTOFADMANgCsQA"
+
+#                     },
+#                "EN":
+#                     {
+#                         "CS":"DYQwdg5gvApmBkpJQMYGd5oPYCcAuUSEAIiHjAIIAmVMV8eAngA4xQC2WAbgJYzy5aOKLTQp4VEIzRQAzADYArEA",
+#                         "EN":"DYQwdg5gvApmBkpKwQZwPYCcAuUkQBERsYBBAE3JnPmwE8AHGKAW3QDcBLGeLKzKFVQBjeORB1UUAMwA2AKxA",
+#                         "SK":"DYQwdg5gvApmBkpJQM4Gt4oPYCcAuUSEAIiHjAIIAmVMV8eAngA4xQC2WAbgJYzy5aOKLRQBjeFRCMUUAMwA2AKxA"
+#                     },
+#                "SK":
+#                     {
+#                         "CS":"DYQwdg5gvAzg1gMlJKBjGCYHsBOAXKZCAERDwFMBBAE2vOoTwE8AHcqAWywDcBLchLjo4odGKgTUQTGFADMANgCsQA",
+#                         "EN":"DYQwdg5gvAzg1gMlJKBTMCYHsBOAXKZCAERD1QEEATK1KhPATwAdUoBbLANwEtUFctHFFowAxgiohGMKAGYAbAFYgA",
+#                         "SK":"DYQwdg5gvAzg1gMlJWiYHsBOAXKyIAiI2ApgIIAmFJFC2AngA4lQC26AbgJYkJbWYo1GAGMEFEPRhQAzADYArEA"
+#                     }
+#               }
+
+
 addon = xbmcaddon.Addon("plugin.video.stream-cinema-2-release")
 
 preferred_language = int(addon.getSetting("preferred_language"))
@@ -34,6 +37,8 @@ else:
 preferred_language = languages[preferred_language]
 fallback_language = languages[fallback_language]    
 
-xbmcgui.Window(10000).setProperty("SCC_new_dub_movies", "plugin://plugin.video.stream-cinema-2-release/get_media/?media_type=movie&amp;render_type=default&amp;url=%2fapi%2fmedia%2ffilter%2fnewsDubbed%3fquery%3d" + new_dub_url[preferred_language][fallback_language])
-xbmcgui.Window(10000).setProperty("SCC_fallback_language", "plugin://plugin.video.stream-cinema-2-release/get_media/?media_type=tvshow&amp;render_type=default&amp;url=%2fapi%2fmedia%2ffilter%2fnewsDubbed%3fquery%3d" + new_dub_url[preferred_language][fallback_language])
+# xbmcgui.Window(10000).setProperty("SCC_new_dub_movies", "plugin://plugin.video.stream-cinema-2-release/get_media/?media_type=movie&amp;render_type=default&amp;url=%2fapi%2fmedia%2ffilter%2fnewsDubbed%3fquery%3d" + new_dub_url[preferred_language][fallback_language])
+# xbmcgui.Window(10000).setProperty("SCC_new_dub_tvshows", "plugin://plugin.video.stream-cinema-2-release/get_media/?media_type=tvshow&amp;render_type=default&amp;url=%2fapi%2fmedia%2ffilter%2fnewsDubbed%3fquery%3d" + new_dub_url[preferred_language][fallback_language])
+xbmcgui.Window(10000).setProperty("SCC_preferred_language", preferred_language)
+xbmcgui.Window(10000).setProperty("SCC_fallback_language", fallback_language)
 
